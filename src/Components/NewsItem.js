93b214b7083ef7477c2ded0338a95f2746b3c5cf
asim571 <160458924+asim571@users.noreply.github.com>
2024-03-27 16:36:34 +0545

    first commit

---
+++ src/Components/NewsItem.js
@@ -0,0 +1,40 @@
+import React, { Component } from "react";
+export class NewsItem extends Component {
+  render() {
+    let { title, description, imageUrl, newsUrl, author, date, source } = this.props;
+    return (
+      <div className="my-3">
+        <div className="card">
+        <span className="position-absolute top-0 translate-middle badge rounded-pill bg-secondary" style={{left: '80%', zIndex:'1'}}>
+                {source}
+              </span>
+          <img
+            src={
+              !imageUrl
+                ? "https://0043d976.rocketcdn.me/wp-content/uploads/2024/02/how-to-invest-in-spacex-1.jpg"
+                : imageUrl
+            }
+            className="card-img-top"
+            alt="..."
+          />
+          <div className="card-body">
+            <h5 className="card-title">
+              {title}...{" "}
+            </h5>
+            <p className="card-text">{description}...</p>
+            <p className="card-text">
+              <small className="text-body-secondary">
+                By {!author ? "Unknown" : author} on{" "}
+                {new Date(date).toGMTString()}
+              </small>
+            </p>
+            <a href={newsUrl} target="_blank" className="btn btn-sm btn-dark">
+              Read More ...
+            </a>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+export default NewsItem;
